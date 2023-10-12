56428ff592143595e4bce6750bf8448876dde833
Anton <sokolanton.1998@gmail.com>
2023-10-12 15:36:43 +0300

    vanilla redux version

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { Home } from "./pages/Home/Home";
 import { Calculator } from "./pages/Calculator/Calculator";
 import { TicTacToe } from "./pages/TicTacToe/TicTacToe";
 import { Routes, Route, NavLink } from "react-router-dom";
+import "./main.css";
 
 function App() {
   return (
     <>
-      <nav>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/calculator">Calculator</NavLink>
-        <NavLink to="/tictactoe">Tic-Tac-Toe</NavLink>
-      </nav>
+      <header className="header">
+        <nav className="nav">
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/calculator">Calculator</NavLink>
+          <NavLink to="/tictactoe">Tic-Tac-Toe</NavLink>
+        </nav>
+      </header>
       <Routes>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/calculator" element={<Calculator />}></Route>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,12 @@
 export function Home() {
-  return <h1>hello</h1>;
+  return (
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
+      exercitationem aliquam sit saepe ad harum nihil possimus sapiente culpa
+      odit deleniti voluptatum aspernatur assumenda officia porro, illum quasi
+      odio obcaecati recusandae accusantium dolorem veniam est adipisci rerum.
+      Porro nisi, voluptates vel harum perferendis nam debitis nemo dicta labore
+      voluptatibus voluptatem!
+    </p>
+  );
 }
